f80ef0469d3608627099ed36e23e849c93003119
armin azhdehnia <armin.azhdehnia@gmail.com>
2021-06-24 00:32:31 -0700

    add manage.py

---
+++ manage.py
@@ -0,0 +1,15 @@
+import argparse
+
+
+def main(args) -> None:
+    pass
+
+
+if __name__ == '__main__':
+    # Start arg parser
+    parser = argparse.ArgumentParser()
+
+    args = parser.parse_args()
+    # End arg parser
+
+    main(args)
