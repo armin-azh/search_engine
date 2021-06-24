d575ddd1671719be57b78da639752ad24201e2d3
armin azhdehnia <armin.azhdehnia@gmail.com>
2021-06-24 01:13:05 -0700

    add configparser into settings

--- settings.py
+++ settings.py
@@ -4,8 +4,12 @@ from __future__ import division
 
 import os
 import pathlib
+import configparser
+
+conf = configparser.ConfigParser()
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+conf.read(os.path.join(BASE_DIR, "conf.ini"))
 
 DATA_DIR = pathlib.Path(BASE_DIR).joinpath("data")
 DATA_DIR.mkdir(exist_ok=True)
