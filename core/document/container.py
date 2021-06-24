2fc4cc58f1272682aa74aab7faa80d1d10a00d34
armin azhdehnia <armin.azhdehnia@gmail.com>
2021-06-24 16:19:43 -0700

    merge with main

---
+++ core/document/container.py
@@ -0,0 +1,12 @@
+class Document:
+    _d_id = 1
+
+    def __init__(self,reference:str):
+        self._doc_id = Document.assign_new_id()
+        self._doc_ref = reference
+
+    @classmethod
+    def assign_new_id(cls) -> str:
+        _id = str(cls._d_id)
+        cls._d_id += 1
+        return _id
